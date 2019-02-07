6167ff40a115e63ec725b4c9f923131eea938177
mnm117 <38852156+mnm117@users.noreply.github.com>
2019-02-07 22:26:16 +0330

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+
+
+
+
+public class HelloWorld {
+    public static void main(String[] args) {
+      LocalTime currentTime = new LocalTime();
+		  System.out.println("Hello world");
+
+        
+    }
+}
